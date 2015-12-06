2d2477827694a670d6b60fc89881319768f9c9c3
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-12-06 14:39:53 -0500

    Added first implementation of ParametricModel class and subclasses, also added some docstrings

--- stingray/parametricmodels.py
+++ stingray/parametricmodels.py
@@ -600,7 +600,6 @@ class FixedCentroidLorentzian(ParametricModel):
         amplitude_min= hyperpars["amplitude_min"]
         amplitude_max = hyperpars["amplitude_max"]
 
-
         def logprior(gamma, amplitude):
 
             assert np.isfinite(gamma), "gamma must be finite"
