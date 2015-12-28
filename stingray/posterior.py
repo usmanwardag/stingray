b6159f63b8e22ee3651bd715a4ab1e61d32dc16a
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-12-28 14:31:21 +0100

    Added Poisson posterior for lightcurves

--- stingray/posterior.py
+++ stingray/posterior.py
@@ -111,9 +111,15 @@ class PSDPosterior(Posterior):
                for a maximum likelihood-style analysis, no prior is required.
 
         """
-
+        self.ps = ps
         self.m = ps.m
-        Posterior.__init__(self, ps.freq, ps.ps, model)
+        Posterior.__init__(self,ps.freq, ps.ps, model)
+
+    def logprior(self, t0):
+        """
+        The logarithm of the prior distribution for the
+        model defined in self.model.
+	"""
 
 
 
@@ -199,6 +205,29 @@ class LightcurvePosterior(Posterior):
         self.lc = lc
         Posterior.__init__(self, lc.time, lc.counts, model)
 
+
+
+    def logprior(self, t0):
+        """
+        The logarithm of the prior distribution for the
+        model defined in self.model.
+
+        Parameters:
+        ------------
+        t0: {list | numpy.ndarray}
+            The list with parameters for the model
+
+        Returns:
+        --------
+        logp: float
+            The logarithm of the prior distribution for the model and
+            parameters given.
+        """
+        assert hasattr(self.model, "logprior")
+        assert np.size(t0) == self.model.npar, "Input parameters must " \
+                                               "match model parameters!"
+
+        return self.model.logprior(*t0)
 
 
     def loglikelihood(self, t0, neg=False):
